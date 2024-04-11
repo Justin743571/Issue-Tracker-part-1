1c0d1b1042d260d03f2ba831eadb34b6bcd477dc
Wang Xu <wxu743571@gmail.com>
2024-04-11 14:19:53 +0800

    实现客户端验证

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,19 +1,14 @@
+import { createIssueSchema } from "@/app/ValidationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-
-const schema = z.object({
-  title: z.string().min(1,'title is required').max(255),
-  description: z.string().min(1,'description is required'),
-});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = schema.safeParse(body);
+  const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const newIssue =  await prisma.issue.create({
+  const newIssue = await prisma.issue.create({
     data: {
       title: body.title,
       description: body.description,
